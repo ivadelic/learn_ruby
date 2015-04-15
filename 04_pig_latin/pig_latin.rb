ad9e8a969b700cb4679518976e1d193c516a320b
Iva Delic <ms.iva.delic@gmail.com>
2015-04-15 18:47:03 -0400

    Pig latin now works with q and u

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,12 +1,33 @@
-Alpha = ('a'..'z').to_a
-Vowel = %w[a e i o u]
-Consonants = Alpha-Vowel
-	
-	# pig = word.split 
+VOWELS = %w[a e i o u]
 
-def translate(word)
-	if Vowel.include?(word[0])  #begins with vowel
-		word+"ay"
-	else 
+# "eat" => "eatay"
+# "een" => "eenquay"
+# "the fox like socks" => "ethay oxfay ikelay ockssay"
+
+def tokenize(string)
+	string.split(" ")
+end
+
+def vowel?(char)
+	VOWELS.include?(char)
+end
+
+def is_qu?(chars)
+	chars[0,2] == ['q', 'u']
+end
+
+def translate_word(word)
+	chars = word.chars
+	until vowel?(chars.first)
+		chars.rotate!(1) if is_qu?(chars)
+		chars.rotate!(1)
 	end
+	"#{chars.join}ay"
+end
+
+def translate(sentence)
+	result = tokenize(sentence).map do |word|
+		translate_word(word)
+	end
+	result.join(" ")
 end
