c33568a1bcac8cb76cc732778aeb0b90037dd004
Iva Delic <ms.iva.delic@gmail.com>
2015-05-18 15:04:27 -0400

    Finished book titles

--- 07_book_titles/book.rb
+++ 07_book_titles/book.rb
@@ -1,18 +1,20 @@
-NO_CAPITALIZE = %w(and in the of a an over)
-
 class Book
-	def title=(title)
-		@title = title.split
-		@title = title.capitalize
-		if 
-		else
 
-	end
-	
-	def title
-		@title 
-	end
+  attr_reader :title
+
+	def title=(title)
+    mini_words = %w(a an to in and of the)
+		maxi_words = title.split(" ").map do |word|
+      if mini_words.include?(word)
+        word
+      else
+        word.capitalize
+      end
+    end
+    maxi_words.first.capitalize!
+    @title = maxi_words.join(" ")
+  end
 end
 
-# book = Book.new 
-# book.title = "inferno" 
+# book = Book.new
+# book.title = "inferno"
